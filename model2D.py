4c74a666ae14be6cc0da25822d9201ba45f8e5d0
Hanzhe Teng <hteng007@ucr.edu>
2019-05-11 11:56:20 -0700

    better structure

--- model2D.py
+++ model2D.py
@@ -12,54 +12,63 @@ import matplotlib.pyplot as plt
 
 class Model2D:
 	def __init__(self):
-		rospy.init_node('model2D')
+		#rospy.init_node('model2D')
 		#rospy.Subscriber("input2D", Vector3, self.callback)
 		#self.state = rospy.Publisher('state2D',Transform,queue_size = 1)
 		#msg_pub = Transform()
 		#rospy.spin()
-		g = 9.8
-		Ixx = 1.43*10.0**(-5.0)   # ** instead of ^ for exponentiation
-		m = 0.03
+		q0 = np.array([2, 0])  # y, z - you cannot track phi in 2d case
+		qh = np.array([5, 10]) # y, z - desired pose
+		zt = np.array([8])     # take-off height
+		T  = np.array([100])    # hovering time in second; taking value from 50 to 150
 
-		N = 3001 # steps
-		t = np.linspace(0,300,N)
-
-		self.x = np.zeros((N,6))  # index 0-5
-		self.u = np.zeros((N,2))  # index 0-1
-
-		self.x[0,:] = np.array([0,0,0,0,0,0]) # init state
-		self.u[0,:] = np.array([0,0])
+		self.N = 3001 # steps
+		self.time = np.linspace(0,300,self.N)
 		
-		# Solve ODE
-		for i in range(1,N):
+		self.xr = np.zeros((self.N,6))
+		self.getXr(q0, qh, zt, T) # generate reference trajectory
+
+		self.x = np.zeros((self.N,6))  # index 0-5
+		self.u = np.zeros((self.N,2))  # index 0-1
+
+		self.x[0,:] = self.xr[0,:]    # set init state to reference state
+		self.u[0,:] = np.array([0,0])
+
+		
+		# simulation starts
+		for i in range(1,self.N):
 			# calculate control input
-			self.controller(step=i, t=t[i])
+			self.controller(step=i)
 
 			# span for next time step
-			tspan = [t[i-1],t[i]] 
+			tspan = [self.time[i-1],self.time[i]] 
 
 			# solve for next step
 			output = odeint(self.model, self.x[i-1,:], tspan, args=(self.u[i-1,:],)) 
 			self.x[i,:] = output[1,:]  # take the value at time i and drop that at time i-1
 
 		# plot results
-		plt.plot(t,self.u[:,0],'m-',label='u1(t)')
-		plt.plot(t,self.u[:,1],'k-',label='u2(t)')
-		plt.plot(t,self.x[:,0],'r-',label='y(t)')
-		plt.plot(t,self.x[:,1],'g-',label='z(t)')
-		plt.plot(t,self.x[:,2],'b-',label='phi(t)')
+		plt.plot(self.time,self.u[:,0],'m-',label='u1(t)')
+		plt.plot(self.time,self.u[:,1],'k-',label='u2(t)')
+		plt.plot(self.time,self.x[:,0],'r-',label='y(t)')
+		plt.plot(self.time,self.x[:,1],'g-',label='z(t)')
+		plt.plot(self.time,self.x[:,2],'b-',label='phi(t)')
+		plt.plot(self.time,self.xr[:,0], 'r--',label='yr(t)')
+		plt.plot(self.time,self.xr[:,1], 'g--',label='zr(t)')
 		plt.ylabel('values')
 		plt.xlabel('time')
 		plt.legend(loc='best')
 		plt.show()
 
-	# function that returns dz/dt
+	# 2d quadrotor model to be integrated
 	def model(self,x,t,u):
+		# x[0] - y ; x[1] - z ;  x[2] - phi
+		# x[3] - y`; x[4] - z`;  x[5] - phi`
 		g = 9.8
 		Ixx = 1.43*10.0**(-5.0)
 		m = 0.03
 
-		y_dot = x[3]   # index 0-5
+		y_dot = x[3]
 		z_dot = x[4]
 		phi_dot = x[5]
 		y_ddot = -g * x[2]
@@ -69,67 +78,70 @@ class Model2D:
 		dxdt = [y_dot, z_dot, phi_dot, y_ddot, z_ddot, phi_ddot]
 		return dxdt
 
-	def controller(self, step, t):
-		x = self.x[step-1,:]
-		Ixx = 1.43*10.0**(-5.0)
-		kdphi = 4.0
-		kpphi = 6.4
 
-		y0 = 2.0
-		yh = 5.0
-		zh = 10.0
-		zt = 8.0
-
-		m = 0.03
-		g = 9.8
-		kdz = .0
-		kpz = 6.4
-		kdy = 0.5
-		kpy = 0.8
+	def getXr(self, q0, qh, zt, T):
+		y0 = q0[0]
+		z0 = q0[1]
+		yh = qh[0]
+		zh = qh[1]
 
 		t1 = 50.0
 		t2 = 100.0
-		t3 = 150.0
-		T = 100.0
-		t5 = 300.0
+		t3 = 300.0
 
-		# Generate the reference
-		if t<t1:
-			y = y0
-			y_dot = 0.0
-			z = 0.0
-			z_dot = 0.0
-		elif t<t2:
-			y = y0
-			y_dot = 0.0
-			z = zt/(t2-t1)*(t-t1)
-			z_dot = zt/(t2-t1)
-		elif t<t3:
-			y = y0+(yh-y0)/(t3-t2)*(t-t2)
-			y_dot = (yh-y0)/(t3-t2)
-			z = zt+(zh-zt)/(t3-t2)*(t-t2)
-			z_dot = (zh-zt)/(t3-t2)
-		elif t<(t3+T):
-			y = yh
-			y_dot = 0.0
-			z = zh 
-			z_dot = 0.0
-		elif t<t5:
-			y = yh
-			y_dot = 0.0
-			z = zh/(t5-t3-T)*(t5-t)
-			z_dot = -zh/(t5-t3-T)
-		else:
-			y = yh
-			y_dot = 0.0
-			z = 0.0
-			z_dot = 0.0
+		for step in range(self.N):
+			t = self.time[step]
+			if t<t1:   # take off
+				y = y0
+				y_dot = 0.0
+				z = zt/t1*t
+				z_dot = zt/t1
+			elif t<t2:   # go to desired pose
+				y = y0+(yh-y0)/(t2-t1)*(t-t1)
+				y_dot = (yh-y0)/(t2-t1)
+				z = zt+(zh-zt)/(t2-t1)*(t-t1)
+				z_dot = (zh-zt)/(t2-t1)
+			elif t<(t2+T):  # hover for T second
+				y = yh
+				y_dot = 0.0
+				z = zh 
+				z_dot = 0.0
+			elif t<t3:   # land
+				y = yh
+				y_dot = 0.0
+				z = zh/(t3-t2-T)*(t3-t)
+				z_dot = -zh/(t3-t2-T)
+			else:
+				y = yh
+				y_dot = 0.0
+				z = 0.0
+				z_dot = 0.0
+			self.xr[step,:] = [y, z, 0, y_dot, z_dot, 0] # phi = 0
+
+
+	def controller(self, step):
+		m = 0.03
+		g = 9.8
+		Ixx = 1.43*10.0**(-5.0)
+
+		kdy = 0.5
+		kpy = 0.8
+		kdz = 4.0
+		kpz = 6.4
+		kdphi = 4.0
+		kpphi = 6.4
+
+		x = self.x[step-1,:] # state
+		y = self.xr[step-1,0] # reference
+		z = self.xr[step-1,1] # reference
+		y_dot = self.xr[step-1,3] # reference
+		z_dot = self.xr[step-1,4] # reference
 
 		# Position Controller
 		phi = -1/g*(kdy*(y_dot-x[3])+kpy*(y-x[0]))
 		phi_dot = -kdy*x[2]-1/g*kpy*(y_dot-x[3])
 		phi_ddot = -kdy*x[5]+kpy*x[2]
-		self.u1 = m*(g+kdz*(z_dot-x[4])+kpz*(z-x[1]))
+		self.u1 = m*( g + kdz*(z_dot-x[4]) + kpz*(z-x[1]) )
 		self.phi_des = np.array([phi, phi_dot, phi_ddot])
 
 		# Attitude Controller
